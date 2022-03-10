01aa4d4dcbd531fa27f8530a4476b9d42fdbc274
Full Stack Developer <webdeveloper2009@outlook.com>
2022-03-10 05:24:59 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Clock />
+    </div>
+  );
+}
